a4fc7b6723b24f1a1954f00c5d6d80579894783f
Anna Debenham <anna@maban.co.uk>
2015-04-26 15:33:52 +0100

    Implement issue #11

--- _tmpl/index.php
+++ _tmpl/index.php
@@ -1,4 +1,4 @@
-<?php include_once('functions.php'); 
+<?php include_once('functions.php');
     // Build out URI to reload from form dropdown
     $pageURL = (@$_SERVER["HTTPS"] == "on") ? "https://" : "http://";
     if (isset($_POST['uri']) && isset($_POST['section'])) {
@@ -11,7 +11,7 @@
 <!doctype HTML>
 <html lang="en">
 <head>
-    
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -19,7 +19,7 @@
     <title>A List Apart Pattern Library</title>
 
     <link rel="stylesheet" href="css/pattern-lib.css" />
-    
+
 </head>
 
 <body class="xx">
@@ -29,31 +29,32 @@
     <?php else : ?>
 
         <section class="main-content">
-        
+
             <h1 class="xx-title">A Pattern Apart</h1>
             <p class="xx-subtitle">A List Apart's pattern library</p>
 
             <div class="global-nav deluxe xx-nav">
-                
+
                 <ul class="inline-items">
                     <li><a href="http://alistapart.com">Home</a></li>
                     <li><a href="patchwork.php">View as patchwork</a></li>
+                    <li><a href="https://github.com/alistapart/pattern-library">Github Repo</a></li>
                 </ul>
-                
+
                 <form action="" method="post" id="pattern" class="pattern-jump">
                     <select name="section" id="pattern-select" class="nav-section-select">
                         <option value="">Jump to&#8230;</option>
                         <?php displayOptions($patternsPath); ?>
-                        
+
                     </select>
                     <input type="hidden" name="uri" value="<?php echo $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"]; ?>">
                     <button type="submit" id="pattern-submit">Go</button>
                 </form>
-                
+
             </div>
-            
+
             <main role="main">
-                
+
                 <div class="pattern">
                 	<div class="pattern-details">
                 	    <h3 class="pattern-name">Swatches</h3>
@@ -104,11 +105,11 @@
         		    	</ul>
         		    </div>
         		</div>
-        		
+
                 <?php displayPatterns($patternsPath); ?>
-            
+
             </main>
-        
+
         </section>
 
     <?php endif; ?>
@@ -116,4 +117,4 @@
 
 <script src="js/pattern-lib.js"></script>
 
-</html> 
+</html>
